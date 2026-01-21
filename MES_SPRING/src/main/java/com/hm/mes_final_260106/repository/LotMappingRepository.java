392e7320ef48f425893919cc18c55c330c81f067
29kinc <29kinc@gmail.com>
2026-01-21 15:29:29 +0900

    ItemLot -> LotMapping 으로

--- MES_SPRING/src/main/java/com/hm/mes_final_260106/repository/LotMappingRepository.java
+++ MES_SPRING/src/main/java/com/hm/mes_final_260106/repository/LotMappingRepository.java
@@ -3,5 +3,5 @@ package com.hm.mes_final_260106.repository;
 import com.hm.mes_final_260106.entity.LotMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemLotRepository extends JpaRepository<LotMapping, Long> {
+public interface LotMappingRepository extends JpaRepository<LotMapping, Long> {
 }
