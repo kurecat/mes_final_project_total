8f7d6a7604881b3bb86a9c42fda3851d2b4b855e
29kinc <29kinc@gmail.com>
2026-01-21 15:23:17 +0900

    중간커밋

---
+++ MES_SPRING/src/main/java/com/hm/mes_final_260106/entity/LotMapping.java
@@ -0,0 +1,26 @@
+package com.hm.mes_final_260106.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "item_lot")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LotMapping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "production_log_id", nullable = false)
+    private ProductionLog productionLog;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lot_id", nullable = false)
+    private Lot lot;
+}
+
